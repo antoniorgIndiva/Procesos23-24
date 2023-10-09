8c1e5cf74fa14778b84ec3044954c0495206db9d
ANTONIO ROMERO GARCÍA <Antonio.Romero13@alu.uclm.es>
2023-10-09 12:26:23 +0200

    Control web y boton au

--- cliente/clienteRes.js
+++ cliente/clienteRes.js
@@ -10,4 +10,53 @@ function ClienteRest(){
             }
         });
     }
+    this.agregarUsuario2=function(nick){
+        $.ajax({
+            type:'GET',
+            url:'/agregarUsuario/'+nick,
+            success:function(data){
+            if (data.nick!=-1){
+                console.log("Usuario "+nick+" ha sido registrado")
+            }
+            else{
+                console.log("El nick ya está ocupado");
+            }
+            },
+            error:function(xhr, textStatus, errorThrown){
+                console.log("Status: " + textStatus);
+                console.log("Error: " + errorThrown);
+            },
+            contentType:'application/json'
+        });
+    }
+    this.obtenerUsuarios=function(){
+        $.getJSON("/obtenerUsuarios",function(data){
+            console.log(data);
+        }); 
+    }
+    this.numeroUsuarios=function(){
+        $.getJSON("/numeroUsuarios",function(data){
+            console.log('Número de usuarios en el sistema es: '+data.num);
+        });
+    }
+    this.usuarioActivo=function(nick){
+        $.getJSON("/usuarioActivo/"+nick,function(data){
+            if (data.activo){
+                console.log("El usuario "+nick+" está activo");
+            }
+            else{
+                console.log("El usuario "+nick+" NO está activo")
+            }
+        });
+    }
+    this.eliminarUsuario=function(nick){
+        $.getJSON("/eliminarUsuario/"+nick,function(data){
+            if (data.nick!=-1){
+                console.log("El usuario "+nick+" ha sido eliminado");
+            }
+            else{
+                console.log("El usuario "+nick+" no se ha podido eliminar")
+            }
+        });
+    }
 }
---
+++ cliente/controlWeb.js
@@ -0,0 +1,24 @@
+function ControlWeb(){
+    this.mostrarAgregarUsuario = function(){
+        $('#mAU').remove();
+       let cadena=` <div id="mAU" class="form-group">
+        <label for="nick">Introduce el nick:</label>
+        <input type="text" class="form-control" id="nick">
+        <button id="btnAU" type="submit" class="btn btn-primary">Submit</button>
+        </div>`
+        $("#au").append(cadena);
+
+        $("#btnAU").on("click", function(){
+            let nick = $("#nick").val()
+            if(nick){
+                $('#mAU').remove();
+                rest.agregarUsuario(nick)
+            }
+            //recoger valor input text
+            //llamar al servidor usando rest
+            
+        })
+
+    }
+
+}
