c15109978aa22241c6f3c513bf5ab8765444658b
ANTONIO ROMERO GARCÍA <Antonio.Romero13@alu.uclm.es>
2023-10-16 18:54:38 +0200

    actualizada url de callback

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -11,7 +11,8 @@ passport.use(new GoogleStrategy({
  clientID:"259650379862-gb7aapmqs4nrc3rh1gc11ioqlmtaj2gd.apps.googleusercontent.com", //la de despliegue
 //  clientSecret: "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",
 clientSecret:"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", //la de despliegue
- callbackURL: "http://localhost:3000/google/callback"
+//  callbackURL: "http://localhost:3000/google/callback"
+callbackURL:"https://arqbase-gh-yw5dam37vq-ew.a.run.app/google/callback"
 },
 function(accessToken, refreshToken, profile, done) {
 return done(null, profile);
