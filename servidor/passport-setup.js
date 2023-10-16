187fce46a4860a5c804e4d39fbbcb31671cebc0f
ANTONIO ROMERO GARCÍA <Antonio.Romero13@alu.uclm.es>
2023-10-16 18:50:15 +0200

    autenticacion google despliegue

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -7,8 +7,10 @@ passport.deserializeUser(function(user, done) {
  done(null, user);
 });
 passport.use(new GoogleStrategy({
- clientID: "259650379862-n012h81tuq3dggb9dld7kmo2prtr4ole.apps.googleusercontent.com",
- clientSecret: "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",
+//  clientID: "259650379862-n012h81tuq3dggb9dld7kmo2prtr4ole.apps.googleusercontent.com",
+ clientID:"259650379862-gb7aapmqs4nrc3rh1gc11ioqlmtaj2gd.apps.googleusercontent.com", //la de despliegue
+//  clientSecret: "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",
+clientSecret:"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", //la de despliegue
  callbackURL: "http://localhost:3000/google/callback"
 },
 function(accessToken, refreshToken, profile, done) {
