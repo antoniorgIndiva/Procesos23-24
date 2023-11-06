7cd4b0f84415ed6c83bd2d139e9fac1de265b569
ANTONIO ROMERO GARCÍA <Antonio.Romero13@alu.uclm.es>
2023-11-06 09:54:36 +0100

    Validaciones modal

--- cliente/clienteRes.js
+++ cliente/clienteRes.js
@@ -1,146 +1,156 @@
 
-function ClienteRest(){
-    this.agregarUsuario=function(nick){
-        var cli=this;
-        $.getJSON("/agregarUsuario/"+nick,function(data){
-        let msg="El nick "+nick+" está ocupado";
-        if (data.nick!=-1){
-        console.log("Usuario "+nick+" ha sido registrado");
-        msg="Bienvenido al sistema, "+nick;
-        $.cookie(clave, valor)
+function ClienteRest() {
+  this.agregarUsuario = function (nick) {
+    var cli = this;
+    $.getJSON("/agregarUsuario/" + nick, function (data) {
+      let msg = "El nick " + nick + " está ocupado";
+      if (data.nick != -1) {
+        console.log("Usuario " + nick + " ha sido registrado");
+        msg = "Bienvenido al sistema, " + nick;
+        $.cookie(clave, valor);
         // localStorage.setItem("nick",nick);
-        }
-        else{
+      } else {
         console.log("El nick ya está ocupado");
+      }
+      cw.mostrarMensaje(msg);
+    });
+  };
+  this.agregarUsuario2 = function (nick) {
+    $.ajax({
+      type: "GET",
+      url: "/agregarUsuario/" + nick,
+      success: function (data) {
+        if (data.nick != -1) {
+          console.log("Usuario " + nick + " ha sido registrado");
+        } else {
+          console.log("El nick ya está ocupado");
         }
-        cw.mostrarMensaje(msg);
-        });
-        }
-    this.agregarUsuario2=function(nick){
-        $.ajax({
-            type:'GET',
-            url:'/agregarUsuario/'+nick,
-            success:function(data){
-            if (data.nick!=-1){
-                console.log("Usuario "+nick+" ha sido registrado")
-            }
-            else{
-                console.log("El nick ya está ocupado");
-            }
-            },
-            error:function(xhr, textStatus, errorThrown){
-                console.log("Status: " + textStatus);
-                console.log("Error: " + errorThrown);
-            },
-            contentType:'application/json'
-        });
-    }
-    this.obtenerUsuarios=function(){
-        $.getJSON("/obtenerUsuarios",function(data){
-            console.log(data);
-        }); 
-    }
-    this.numeroUsuarios=function(){
-        $.getJSON("/numeroUsuarios",function(data){
-            console.log('Número de usuarios en el sistema es: '+data.num);
-        });
-    }
-    this.usuarioActivo=function(nick){
-        $.getJSON("/usuarioActivo/"+nick,function(data){
-            if (data.activo){
-                console.log("El usuario "+nick+" está activo");
-            }
-            else{
-                console.log("El usuario "+nick+" NO está activo")
-            }
-        });
-    }
-    this.eliminarUsuario=function(nick){
-        $.getJSON("/eliminarUsuario/"+nick,function(data){
-            if (data.nick!=-1){
-                console.log("El usuario "+nick+" ha sido eliminado");
-            }
-            else{
-                console.log("El usuario "+nick+" no se ha podido eliminar")
-            }
-        });
-    }
-    this.enviarJwt=function(jwt){
-        $.ajax({
-        type:'POST',
-        url:'/enviarJwt',
-        data: JSON.stringify({"jwt":jwt}),
-        success:function(data){
-        let msg="El nick "+ data.nick+" está ocupado";
-        if (data.nick!=-1){
-        console.log("Usuario "+data.nick+" ha sido registrado");
-        msg="Bienvenido al sistema, "+data.nick;
-        $.cookie("nick",data.nick);
-        
-        }
-        else{
-            console.log("El nick ya está ocupado");
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log("Status: " + textStatus);
+        console.log("Error: " + errorThrown);
+      },
+      contentType: "application/json",
+    });
+  };
+  this.obtenerUsuarios = function () {
+    $.getJSON("/obtenerUsuarios", function (data) {
+      console.log(data);
+    });
+  };
+  this.numeroUsuarios = function () {
+    $.getJSON("/numeroUsuarios", function (data) {
+      console.log("Número de usuarios en el sistema es: " + data.num);
+    });
+  };
+  this.usuarioActivo = function (nick) {
+    $.getJSON("/usuarioActivo/" + nick, function (data) {
+      if (data.activo) {
+        console.log("El usuario " + nick + " está activo");
+      } else {
+        console.log("El usuario " + nick + " NO está activo");
+      }
+    });
+  };
+  this.eliminarUsuario = function (nick) {
+    $.getJSON("/eliminarUsuario/" + nick, function (data) {
+      if (data.nick != -1) {
+        console.log("El usuario " + nick + " ha sido eliminado");
+      } else {
+        console.log("El usuario " + nick + " no se ha podido eliminar");
+      }
+    });
+  };
+  this.enviarJwt = function (jwt) {
+    $.ajax({
+      type: "POST",
+      url: "/enviarJwt",
+      data: JSON.stringify({ jwt: jwt }),
+      success: function (data) {
+        let msg = "El nick " + data.nick + " está ocupado";
+        if (data.nick != -1) {
+          console.log("Usuario " + data.nick + " ha sido registrado");
+          msg = "Bienvenido al sistema, " + data.nick;
+          $.cookie("nick", data.nick);
+        } else {
+          console.log("El nick ya está ocupado");
         }
         cw.limpiar();
         cw.mostrarMsg(msg);
-        },
-        error:function(xhr, textStatus, errorThrown){
+      },
+      error: function (xhr, textStatus, errorThrown) {
         //console.log(JSON.parse(xhr.responseText));
         console.log("Status: " + textStatus);
         console.log("Error: " + errorThrown);
-        },
-        contentType:'application/json'
-        //dataType:'json'
-        });
+      },
+      contentType: "application/json",
+      //dataType:'json'
+    });
+  };
+  this.registrarUsuario = function (email, password) {
+    $.ajax({
+      type: "POST",
+      url: "/registrarUsuario",
+      data: JSON.stringify({ email: email, password: password }),
+      success: function (data) {
+        if (data.nick != -1) {
+          console.log("Usuario " + data.nick + " ha sido registrado");
+          //$.cookie("nick",data.nick);
+          cw.limpiar();
+          Swal.fire({
+            icon: "info",
+            title: "Revise su correo " + email + " para confirmar la cuenta",
+            showConfirmButton: false,
+            timer: 3000,
+          });
+          cw.mostrarLogin();
+        } else {
+          console.log("El nick está ocupado");
         }
-    this.registrarUsuario=function(email,password){
-            $.ajax({
-            type:'POST',
-            url:'/registrarUsuario',
-            data: JSON.stringify({"email":email,"password":password}),
-            success:function(data){
-            if (data.nick!=-1){
-            console.log("Usuario "+data.nick+" ha sido registrado");
-            //$.cookie("nick",data.nick);
-            cw.limpiar();
-            //cw.mostrarMensaje("Bienvenido al sistema, "+data.nick);
-            cw.mostrarLogin();
-            }
-            else{
-            console.log("El nick está ocupado");
-            }
-            },
-            error:function(xhr, textStatus, errorThrown){
-            console.log("Status: " + textStatus);
-            console.log("Error: " + errorThrown);
-            },
-            contentType:'application/json'
-            });
-        }
-        this.loginUsuario=function(email,password){
-            $.ajax({
-            type:'POST',
-            url:'/loginUsuario',
-            data: JSON.stringify({"email":email,"password":password}),
-            success:function(data){
-            if (data.nick!=-1){
-            console.log("Usuario "+data.nick+" ha sido registrado");
-            $.cookie("nick",data.nick);
-            cw.limpiar();
-            cw.mostrarMensaje("Bienvenido al sistema, "+data.nick);
-            //cw.mostrarLogin();
-            }
-            else{
-            console.log("No se pudo iniciar sesión");
-            }
-            },
-            error:function(xhr, textStatus, errorThrown){
-            console.log("Status: " + textStatus);
-            console.log("Error: " + errorThrown);
-            },
-            contentType:'application/json'
-            });
-        }
-    
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log("Status: " + textStatus);
+        console.log("Error: " + errorThrown);
+      },
+      contentType: "application/json",
+    });
+  };
+  this.loginUsuario = function (email, password) {
+    $.ajax({
+      type: "POST",
+      url: "/loginUsuario",
+      data: JSON.stringify({ email: email, password: password }),
+      success: function (data) {
+        if (data.nick != -1) {
+          console.log("Usuario " + data.nick + " ha sido registrado");
+          $.cookie("nick", data.nick);
+          cw.limpiar();
+          cw.mostrarMsg("Bienvenido al sistema, " + data.nick);
+          //cw.mostrarLogin();
+        } else {
+            Swal.fire({
+            icon: 'error',
+            title: 'Contraseña/usuario incorrecto',
+            showConfirmButton: false,
+            timer: 3000
+          });
 
+          console.log("No se pudo iniciar sesión");
+        }
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log("Status: " + textStatus);
+        console.log("Error: " + errorThrown);
+      },
+      contentType: "application/json",
+    });
+    this.datosErroneos=function(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Contraseña/usuario incorrecto',
+            showConfirmButton: false,
+            timer: 3000
+          });
+    }
+  };
 }
--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -1,8 +1,8 @@
-function ControlWeb(){
-    this.mostrarAgregarUsuario = function(){
-        $('#bnv').remove();
-        $('#mAU').remove();
-        let cadena=`<div id="mAU">
+function ControlWeb() {
+  this.mostrarAgregarUsuario = function () {
+    $("#bnv").remove();
+    $("#mAU").remove();
+    let cadena = `<div id="mAU">
         <div class="card"><div class="card-body">
         <div class="form-group">
         <label for="nick">Nick:</label>
@@ -10,102 +10,94 @@ function ControlWeb(){
         <button id="btnAU" type="submit" class="btn btn-primary">Submit</button>
         <div><a href="/auth/google"><img class="mt-2" src="./cliente/img/btn_google_signin_light_focus_web@2x.png" style="height:40px;"></a></div>
         </div>';
-        </div></div></div>`; 
-        $("#au").append(cadena);
+        </div></div></div>`;
+    $("#au").append(cadena);
 
-        $("#btnAU").on("click", function(){
-            let nick = $("#nick").val()
-            if(nick){
-                $('#mAU').remove();
-                rest.agregarUsuario(nick)
-            }
-            //recoger valor input text
-            //llamar al servidor usando rest
-            
-        })
-
-    }
-    this.mostrarMsg=function(msg){
-        $('mMsg').remove()
-        let cadena = '<h6 id="mMsg">'+msg+'</h6>'
-        $('#msg').append(cadena)
-    }
-    this.comprobarSesion=function(){
-        //let nick=localStorage.getItem("nick");
-        let nick=$.cookie("nick");
-        if (nick){
-        cw.mostrarMsg("Bienvenido al sistema, "+nick);
-        }
-        else{
-        // cw.mostrarAgregarUsuario();
-        //cw.mostrarRegistro();
-        cw.init();
-        }
-    }
-    this.salir=function(){
-        localStorage.removeItem("nick");
-        $.removeCookie("nick")
-        location.reload();
-    }
-
-    this.init=function(){
-        let cw=this;
-        google.accounts.id.initialize({
-        client_id:"259650379862-gkh4hvjdp1ku5v4grv1u77name2k9hlv.apps.googleusercontent.com", //prod
-        auto_select:false,
-        callback:cw.handleCredentialsResponse
-        });
-        google.accounts.id.prompt();
-        }
-        this.handleCredentialsResponse=function(response){
-            let jwt=response.credential;
-            //let user=JSON.parse(atob(jwt.split(".")[1]));
-            //console.log(user.name);
-            //console.log(user.email);
-            //console.log(user.picture);
-            rest.enviarJwt(jwt);
-        }
-    this.limpiar=function(){
+    $("#btnAU").on("click", function () {
+      let nick = $("#nick").val();
+      if (nick) {
         $("#mAU").remove();
-        $("#fmLogin").remove();
+        rest.agregarUsuario(nick);
+      }
+      //recoger valor input text
+      //llamar al servidor usando rest
+    });
+  };
+  this.mostrarMsg = function (msg) {
+    $("mMsg").remove();
+    let cadena = '<h6 id="mMsg">' + msg + "</h6>";
+    $("#msg").append(cadena);
+  };
+  this.comprobarSesion = function () {
+    //let nick=localStorage.getItem("nick");
+    let nick = $.cookie("nick");
+    if (nick) {
+      cw.mostrarMsg("Bienvenido al sistema, " + nick);
+    } else {
+      // cw.mostrarAgregarUsuario();
+      //cw.mostrarRegistro();
+      cw.init();
     }
-    this.mostrarRegistro = function () {
-        $("#fmRegistro").remove();
-        $("#fmLogin").remove();
-        $("#registro").load("./cliente/registro.html",function () {
-            $("#btnRegistro").on("click", function(){
-                let email = $("#email").val()
-                let pwd = $("#pwd").val()
-                if(email && pwd){
-                    rest.registrarUsuario(email,pwd)
-                    console.log(email+ " " +pwd )
-                }
-                //recoger valor input text
-                //llamar al servidor usando rest
-                
-            })
-        })
+  };
+  this.salir = function () {
+    localStorage.removeItem("nick");
+    $.removeCookie("nick");
+    location.reload();
+  };
 
-    }
-    this.mostrarLogin = function () {
-        if($.cookie('nick')) return true
-        $("#fmLogin").remove();
-        $("#fmRegistro").remove();
-        $("#login").load("./cliente/login.html",function () {
-            $("#btnLogin").on("click", function(){
-                let email = $("#email").val()
-                let pwd = $("#pwd").val()
-                if(email && pwd){
-                    rest.loginUsuario(email,pwd)
-                    console.log(email+ " " +pwd )
-                }
-          
-                
-            })
-        })
-
-    }
-    
-       
-
+  this.init = function () {
+    let cw = this;
+    google.accounts.id.initialize({
+      client_id:
+        "259650379862-gkh4hvjdp1ku5v4grv1u77name2k9hlv.apps.googleusercontent.com", //prod
+      auto_select: false,
+      callback: cw.handleCredentialsResponse,
+    });
+    google.accounts.id.prompt();
+  };
+  this.handleCredentialsResponse = function (response) {
+    let jwt = response.credential;
+    //let user=JSON.parse(atob(jwt.split(".")[1]));
+    //console.log(user.name);
+    //console.log(user.email);
+    //console.log(user.picture);
+    rest.enviarJwt(jwt);
+  };
+  this.limpiar = function () {
+    $("#mAU").remove();
+    $("#fmLogin").remove();
+  };
+  this.mostrarRegistro = function () {
+    $("#fmRegistro").remove();
+    $("#fmLogin").remove();
+    $("#registro").load("./cliente/registro.html", function () {
+      $("#btnRegistro").on("click", function () {
+        let email = $("#email").val();
+        let pwd = $("#pwd").val();
+        if (email && pwd) {
+          rest.registrarUsuario(email, pwd);
+          console.log(email + " " + pwd);
+        }
+        //recoger valor input text
+        //llamar al servidor usando rest
+      });
+    });
+  };
+  this.mostrarLogin = function () {
+    if ($.cookie("nick")) return true;
+    $("#fmLogin").remove();
+    $("#fmRegistro").remove();
+    $("#registro").load("./cliente/login.html", function () {
+      $("#btnLogin").on("click", function () {
+        let email = $("#email").val();
+        let pwd = $("#pwd").val();
+        if (email && pwd) {
+          rest.loginUsuario(email, pwd);
+          console.log(email + " " + pwd);
+        } else {
+          rest.datosErroneos();
+        }
+      });
+    });
+  };
 }
