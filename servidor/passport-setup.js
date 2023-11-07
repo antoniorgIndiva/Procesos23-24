2ead4f74e2d1aa841de2af737e8ee8b85fca9051
ANTONIO ROMERO GARCÍA <Antonio.Romero13@alu.uclm.es>
2023-11-07 13:27:17 +0100

    cambiar credenciales a despliegue

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -9,13 +9,13 @@ passport.deserializeUser(function (user, done) {
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        "259650379862-n012h81tuq3dggb9dld7kmo2prtr4ole.apps.googleusercontent.com",
-      //  clientID:"259650379862-gb7aapmqs4nrc3rh1gc11ioqlmtaj2gd.apps.googleusercontent.com", //la de despliegue
-      clientSecret: "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",
-      // clientSecret:"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", //la de despliegue
-      callbackURL: "http://localhost:3000/google/callback",
-      // callbackURL:"https://arqbase-gh-yw5dam37vq-ew.a.run.app/google/callback"
+      // clientID:
+      //   "259650379862-n012h81tuq3dggb9dld7kmo2prtr4ole.apps.googleusercontent.com",
+        clientID:"259650379862-gb7aapmqs4nrc3rh1gc11ioqlmtaj2gd.apps.googleusercontent.com", //la de despliegue
+      // clientSecret: "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",
+      clientSecret:"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", //la de despliegue
+      // callbackURL: "http://localhost:3000/google/callback",
+       callbackURL:"https://arqbase-gh-yw5dam37vq-ew.a.run.app/google/callback"
     },
     function (accessToken, refreshToken, profile, done) {
       return done(null, profile);
