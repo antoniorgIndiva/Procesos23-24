1c27e1993392ab761d9f7140d696fcba1fb3978b
ANTONIO ROMERO GARCÍA <Antonio.Romero13@alu.uclm.es>
2023-11-27 18:21:07 +0100

    error en consola

--- cliente/clienteWS.js
+++ cliente/clienteWS.js
@@ -1,14 +1,15 @@
 function ClienteWS() {
   this.socket;
-  this.conectar = function () {
-    this.socket = io.connect();
-    this.lanzarServidorWS();
-  };
-  this.conectar()
+  
   this.lanzarServidorWS = function () {
     let cli = this
     this.socket.on("connect", function () {
       console.log("Usuario conectado al servidor de WebSockets");
     });
   };
+  this.conectar = function () {
+    this.socket = io.connect();
+    this.lanzarServidorWS();
+  };
+  this.conectar()
 }
