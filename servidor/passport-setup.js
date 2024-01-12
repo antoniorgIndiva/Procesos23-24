0d000ac56e616e1e69dd70f1a3485684cc792d90
ANTONIO ROMERO GARCÍA <Antonio.Romero13@alu.uclm.es>
2024-01-12 12:44:35 +0100

    despliegue

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -11,12 +11,12 @@ passport.deserializeUser(function (user, done) {
 passport.use(
   new GoogleStrategy(
     {
-      clientID:"259650379862-kc0j05v4s83djch10c8a382n5qa85761.apps.googleusercontent.com",
-     // clientID:"259650379862-gb7aapmqs4nrc3rh1gc11ioqlmtaj2gd.apps.googleusercontent.com", //la de despliegue
-      clientSecret: "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",
-      //clientSecret:"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", //la de despliegue
-      callbackURL: "http://localhost:3000/google/callback",
-      //callbackURL:"https://arqbase-gh-yw5dam37vq-ew.a.run.app/google/callback"
+      //clientID:"259650379862-kc0j05v4s83djch10c8a382n5qa85761.apps.googleusercontent.com",
+      clientID:"259650379862-gb7aapmqs4nrc3rh1gc11ioqlmtaj2gd.apps.googleusercontent.com", //la de despliegue
+      //clientSecret: "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",
+      clientSecret:"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", //la de despliegue
+      //callbackURL: "http://localhost:3000/google/callback",
+      callbackURL:"https://arqbase-gh-yw5dam37vq-ew.a.run.app/google/callback"
     },
     function (accessToken, refreshToken, profile, done) {
       return done(null, profile);
@@ -26,10 +26,10 @@ passport.use(
 passport.use(
   new GoogleOneTapStrategy(
     {
-      //clientID: "259650379862-oc05utci7tksinh74a2mqfphnu4pm8u1.apps.googleusercontent.com", //despliegue
-      //clientSecret: "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", //despliegue
-      clientID: "259650379862-kc0j05v4s83djch10c8a382n5qa85761.apps.googleusercontent.com", //local
-      clientSecret: "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", //local
+      clientID: "259650379862-oc05utci7tksinh74a2mqfphnu4pm8u1.apps.googleusercontent.com", //despliegue
+      clientSecret: "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", //despliegue
+      //clientID: "259650379862-kc0j05v4s83djch10c8a382n5qa85761.apps.googleusercontent.com", //local
+      //clientSecret: "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", //local
       verifyCsrfToken: false, // whether to validate the csrf token or not
     },
     function (profile, done) {
